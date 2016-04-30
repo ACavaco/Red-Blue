adb4235c387ce96c59a10498899ecba0b8acf6e3
António Cavaco <antoniocavaco@Antonios-MacBook-Pro.local>
2016-04-30 22:12:58 +0100

    novo commit

--- Red-Blue/ViewController.swift
+++ Red-Blue/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AlienBlue: UIImageView!
+    @IBOutlet weak var GimpRed: UIImageView!
+    @IBOutlet weak var HideBlueButton: UIButton!
+    @IBOutlet weak var HideRedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideBlueAction(sender: AnyObject) {
+        AlienBlue.hidden = true
+    }
 
+    @IBAction func HideRedAction(sender: AnyObject) {
+        GimpRed.hidden = true
+    }
+    
+    
 }
 
